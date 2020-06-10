c85e5b1c9c16f0bb6c85964dcb0b6af77d280cad
sujin16 <tnwls9712@ajou.ac.kr>
2020-06-11 00:05:40 +0900

    add DESC limit start

--- src/main/java/kr/or/connect/navermovie/dao/MainPageDaoSqls.java
+++ src/main/java/kr/or/connect/navermovie/dao/MainPageDaoSqls.java
@@ -2,13 +2,16 @@ package kr.or.connect.navermovie.dao;
 
 public class MainPageDaoSqls {
 	
-	public static final String SELECT_COUNT = "SELECT count(*) FROM display_info";
-	public static final String SELECT_PAGING = "SELECT * FROM display_info ORDER BY id DESC limit :start, :limit";
-
+	public static final String SELECT_PROMOTION 
+	="SELECT file_name  FROM file_info WHERE id IN (SELECTflie_ id FROM product_image WHERE TYPE ='th')" ;
 	
-//	public static final String SELECT_ALL = "SELECT role_id, description FROM role order by role_id";
-//	public static final String UPDATE = "UPDATE role SET description = :description WHERE ROLE_ID = :roleId";
-//	public static final String SELECT_BY_ROLE_ID = "SELECT role_id, description FROM role where role_id = :roleId";
-//	public static final String DELETE_BY_ROLE_ID = "DELETE FROM role WHERE role_id = :roleId";
-
+	public static final String SELECT_COUNT_BY_CATEGORY 
+	="SELECT COUNT(*)  FROM product WHERE category_id = :categoryId";
+	
+	public static final String SELECT_MAIN_PRODUCT_BY_CATEGORY_ID
+	="SELECT product.id AS id, description,content, place_name,save_file_name FROM product, display_info ,product_image, file_info"
+	+"WHERE product.id in (SELECT id FROM product WHERE category_id = :categoryId) AND TYPE='ma'"
+	+ "AND display_info.product_id = product.id AND product.id = product_image.product_id AND file_info.id = product_image.file_id"
+	+"ORDER BY id DESC limit :start, :limit";
+	
 }
